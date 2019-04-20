909588b7bb1b69ef85889694c9e30e8463f9ea52
vkhanna92 <43229822+vkhanna92@users.noreply.github.com>
2019-04-20 15:00:47 -0500

    Create ResQue_Demo.py

---
+++ demo/ResQue_Demo.py
@@ -0,0 +1,42 @@
+from tkinter import *
+# Driver code
+
+def clear():
+    # clear the content of text entry box
+    name_field.delete(0, END)
+    course_field.delete(0, END)
+
+
+if __name__ == "__main__":
+    root = Tk()
+
+    # set the background colour of GUI window
+    root.configure(background='light green')
+
+    # set the title of GUI window
+    root.title("registration form")
+
+    # set the configuration of GUI window
+    root.geometry("500x300")
+    heading = Label(root, text="Form", bg="light green")
+
+    # create a Name label
+    name = Label(root, text="Enter Question Body", bg="light green")
+
+    # create a Course label
+    course = Label(root, text="Enter Question Title", bg="light green")
+    heading.grid(row=0, column=1)
+    name.grid(row=1, column=0)
+    course.grid(row=2, column=0)
+
+    name_field = Entry(root)
+    course_field = Entry(root)
+
+    name_field.grid(row=1, column=1, ipadx="100")
+    course_field.grid(row=2, column=1, ipadx="100")
+
+    submit = Button(root, text="Recommend", fg="Black",
+                    bg="Red")
+    submit.grid(row=8, column=1)
+
+    root.mainloop()
